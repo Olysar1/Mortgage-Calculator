26329c2ac25afec2186cfe2532db43c5754aee84
Olysar1 <giorgibarbakadz1@gmail.com>
2023-05-15 17:26:28 +0400

    finish 1st version

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -12,6 +13,14 @@ public class Main {
         float monthlyInterest = annualInterest/100/12;
 
         System.out.print("Period(years): ");
-        int period = scanner.nextInt();
+        byte period = scanner.nextByte();
+        int numberOfPayments = period * 12;
+
+        double result = principal * (monthlyInterest*Math.pow(1+monthlyInterest, numberOfPayments)) / (Math.pow(1+monthlyInterest, numberOfPayments)-1);
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String formattedResult = currency.format(result);
+
+        System.out.println("Your monthly payment will be " + formattedResult);
+
     }
 }
